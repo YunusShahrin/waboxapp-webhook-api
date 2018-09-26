af59a701d75abaac167071f8663b9c4369b2e264
YunusShahrin <43561205+YunusShahrin@users.noreply.github.com>
2018-09-26 15:43:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,20 +8,12 @@ if($method == 'POST'){
 	$event = $json->result->parameters->event;
 	$token = $json->result->parameters->token;
 	$uid = $json->result->parameters->uid;
-	$contactUid = $json->result->parameters->contact[uid];
-	$contactName = $json->result->parameters->contact[name];
-	$contactType = $json->result->parameters->contact[type];
-	$messageDtm = $json->result->parameters->message[dtm];
-	$messageUid = $json->result->parameters->message[uid];
-	$messageCuid = $json->result->parameters->message[cuid];
-	$messageDir = $json->result->parameters->message[dir];
-	$messageType = $json->result->parameters->comessage[type];
-	$messageBody = $json->result->parameters->message[body];
+	$cuid = $json->result->parameters->cuid;
 	$ack = $json->result->parameters->ack;
 	
 	if($event == "message") {
 		
-		$replyMsg = "Event: " . $event . ", Msg: " . $messageBody . ".";
+		$replyMsg = "Event: " . $event . ".";
 	}
 	else if($event == "ack") {
 		$replyMsg = "Event: " . $event . ", Ack: " . $ack . ".";
